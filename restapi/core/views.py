b7274750f5d9ceb2675e51de771e73c831a0c549
jasoncwilley <jason.c.willey@gmail.com>
2018-06-15 14:29:10 -0400

    working ip lookup

--- restapi/core/views.py
+++ restapi/core/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
 
-# Create your views here.
+from django.shortcuts import render
+from django.conf import settings
+import requests
+from github import Github, GithubException
+
+
+def home(request):
+    is_cached = ('geodata' in request.session)
+
+    if not is_cached:
+        ip_address = request.META.get('HTTP_X_FORWARDED_FOR', '')
+        response = requests.get('http://freegeoip.net/json/%s' % ip_address)
+        request.session['geodata'] = response.json()
+
+    geodata = request.session['geodata']
+
+    return render(request, 'core/home.html', {
+        'ip': geodata['ip'],
+        'country': geodata['country_name'],
+        'latitude': geodata['latitude'],
+        'longitude': geodata['longitude'],
+        'api_key': settings.GOOGLE_MAPS_API_KEY,
+        'is_cached': is_cached
+})
